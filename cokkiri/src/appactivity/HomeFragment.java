507e62fe771bfd80fe46a097ba0590338a62d28a
user <user@DESKTOP-ABD82OM>
2018-05-25 20:38:05 +0900

    이걸로..

--- cokkiri/src/appactivity/HomeFragment.java
+++ cokkiri/src/appactivity/HomeFragment.java
@@ -14,12 +14,6 @@ public class HomeFragment {
 	private JLabel label_exchange_value;
 	
 	public HomeFragment(HomeView homeview) {
-
-<<<<<<< HEAD
-=======
-		//Client.sendMsg("hello", PeerList.getPeerList().get(0));
-		//Client.sendMsg("hello", PeerList.getPeerList().get(1));
->>>>>>> branch 'master' of https://github.com/KimSeonBin/Cokkiri.git
 		this.homeview = homeview;
 		this.label_address_value = homeview.getLabel_address_value();
 		this.label_totalcoin_value = homeview.getLabel_totalcoin_value();
